91d4dd1c37b39665fd5251a135c5759c503bf872
Suman Yadav <sumanydv615@gmail.com>
2026-02-20 11:48:41 -0600

    add relation to db

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Book, Author, Country, Address
 
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("author", "rating", )
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
+admin.site.register(Country)
+admin.site.register(Address)
